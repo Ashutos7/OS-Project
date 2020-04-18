31c12838cc44a3d400e0c25f1887a59422a95cc6
Ashutos7 <63494230+Ashutos7@users.noreply.github.com>
2020-04-18 11:35:36 +0530

    Update Assignment.c

--- Assignment.c
+++ Assignment.c
@@ -4,8 +4,8 @@ int a[20];
 int pass,hold,i,n=10;
 char t;
 int b[10]={'a','b','c','d','e','f','g','h','i','j'};
-printf("Enter the Number of Gifts Purchased By The 10
-Students \n"); for(i=0;i<n;i++)
+printf("Enter the Number of Gifts Purchased By The 10 Students \n"); 
+for(i=0;i<n;i++)
 {
 printf("\n %c",b[i]);
 scanf("%d",&a[i]);
@@ -27,5 +27,5 @@ hold=a[i]; a[i]=a[i+1]; a[i+1]=hold;
 for(i=0;i<n;i++)
 {
 printf("%d \n",a[i]);
-
+}
 }
